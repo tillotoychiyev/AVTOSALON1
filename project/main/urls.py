d2428209ddfd6d69b7994da6d97c023c510e31e4
tillotoychiyev <tillotoychiyev5@gmail.com>
2026-04-11 11:02:32 +0500

    8-dars uchun

--- project/main/urls.py
+++ project/main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import home, about, car_detail, car_by_category
+from .views import home, about, car_detail, car_by_category, add_car
 
 urlpatterns = [
     path('', home, name='home'),
     path('category/<int:category_id>/', car_by_category, name='car_by_category'),
     path('about/', about, name='about'),
     path('car/<int:car_id>/', car_detail, name='detail'),
+    path('car/add', add_car, name="add_car"),
 ]
--- project/main/views.py
+++ project/main/views.py
@@ -10,25 +10,35 @@ def home(request: HttpRequest):
 
     context = {
         'cars':cars,
-        'categories':categories
+        'categories':categories,
+        'title': 'INOMARKA AVTOSALON'
     }
     return render(request, 'main/index.html', context)
 
 def car_by_category(request, category_id):
     categories = Category.objects.all()
-    cars = Car.objects.filter(category_id=category_id)
+    category = Category.objects.get(id=category_id)
+    cars = Car.objects.filter(category=category)
     context = {
-           'cars':cars,
-           'categories':categories
+        'cars':cars,
+        'categories':categories,
+        'title':category.name
     }
     return render(request, 'main/index.html', context)
 
 def car_detail(request, car_id: int):
+    categories = Category.objects.all()
     car = Car.objects.get(id=car_id)
     context = {
-        'car':car
+        'categories':categories,
+        'car':car,
+        'title':car.name
     }
     return render(request, 'main/detail.html', context)
+
+def add_car(request: HttpRequest):
+    print(request.POST)
+    return render(request, 'main/add_car.html')
 
 def about(request: HttpRequest):
     return render(request, 'main/about.html')
