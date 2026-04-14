b3a3e62faf131be0ae6b6f75537b304b3b30709f
tillotoychiyev <tillotoychiyev5@gmail.com>
2026-04-14 09:07:15 +0500

    for image

--- project/main/forms.py
+++ project/main/forms.py
@@ -5,4 +5,6 @@ from .models import Car, Category
 class CarForm(forms.ModelForm):
     class Meta:
         model = Car
-        fields = '__all__'
+        # fields = '__all__'
+        # fields = ['name', 'text', 'price', 'image', 'category', 'published']
+        exclude = ['views']
